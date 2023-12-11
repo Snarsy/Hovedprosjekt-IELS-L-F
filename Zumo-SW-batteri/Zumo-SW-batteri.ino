c99d495d87cf3c7e3b1d03003ae985b083e7010d
Snarsy <uleberg.lars@gmail.com>
2023-12-11 15:08:06 +0100

    La til miniprosjekt

--- Zumo-SW-batteri/Zumo-SW-batteri.ino
+++ Zumo-SW-batteri/Zumo-SW-batteri.ino
@@ -3,12 +3,34 @@
 
 Zumo32U4OLED display;
 
+int batterylife = 100;
+int distance = 0;
+int interval = 20;
+
 void setup(){
     display.setLayout21x8();
     display.clear();
 }
 
 void loop(){
-
+    kjor();
+    speedometer();
+    batterylevel();
+    batteryleveldisplay();
 }
 
+void kjor(){//Bilen kjører, ved å enten følge linje eller mønster
+
+}
+void speedometer(){//Sjekker hvor langt bilen har kjørt og hvor fort den kjører
+    //Sett distance
+}
+void batterylevel(){//Sjekker om avstand kjørt er lengre enn vist intervall. vil så redusere batteri
+    if(distance>interval){
+        batterylife--;
+
+    }
+}
+void batteryleveldisplay(){//Viser batterinivå
+    //Vis batterinivå
+}
