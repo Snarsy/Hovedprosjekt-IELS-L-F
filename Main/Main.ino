24d2ebf660123fe7f597bf1ae88aaf47acf3a1f1
Snarsy <uleberg.lars@gmail.com>
2023-12-11 16:20:47 +0100

    idk

--- Main/Main.ino
+++ Main/Main.ino
@@ -24,7 +24,7 @@ int A = 1;
 int16_t firstSpeed = 0;
 int readTime = 100;
 int16_t totalSpeed = 0;
-int16_t negativeTotalSpeed = 0;
+int16_t negativTotalSpeed = 0;
 bool seventyMillis_start, stoppedTimer = false;
 int holdTimerValue, secondsAboveSeventy, aboveSeventyCounter, maxSpeed, distanceAverage, averageSpeed60Sec = 0;
 
@@ -90,7 +90,7 @@ void speedometer() // Måler fart hvert 10.dels sekund. Siden readtime = 100.
         A = 1;
         previousSpeedMillis = speedMillis;
         totalSpeed = abs((lastSpeed - firstSpeed) / 909.70 * 10.996 * 4); // Verdiene er regnet med hvor mange ganger den teller og areal av hjulet.
-        negativeTotalSpeed = totalSpeed * -1;
+        negativTotalSpeed = ((lastSpeed - firstSpeed) / 909.70 * 10.996 * 4);
         speedDistance += totalSpeed / 10; // Deler på 10 siden den teller hvert 1/10 sekund.
         totalDistance += speedDistance;
         distanceAverage += totalSpeed / 10;
@@ -263,6 +263,8 @@ void driveLinePID()
 {
     int16_t position = lineSensors.readLine(lineSensorValues);
 
+    // display.gotoXY(9,4);
+    // display.print(position);
     int16_t error = position - 2000;
     int16_t speedDifference = error / 0.5 + 4 * (error - lastError);
 
@@ -279,6 +281,8 @@ void driveLinePID()
 void driveLineStandard()
 {
     int Read = lineSensors.readLine(lineSensorValues);
+    // display.gotoXY(9, 4);
+    // display.print(Read);
     if (Read < 2000)
     {
         if (Read < 1750)
@@ -356,6 +360,7 @@ void turndeg(int tilverdi)
             break;
         }
     }
+    // display.print("Heeelo");
 }
 
 void whatPattern()
@@ -654,9 +659,11 @@ void statusDisplay()
         break;
     case 2:
         lowBatteryDisplay();
+        // batteryLevelDisplay();
         break;
     case 3:
         chargeBatteryDisplay();
+        // batteryLevelDisplay();
         break;
     case 4:
         emptyBatteryDisplay();
@@ -714,7 +721,7 @@ void batteryCase()
         {
             batteryLife = 100;
         }
-        else if (batteryLife > 10)
+        else if (batteryLife > 10)|
         {
             batteryStatusDisplayVar = 0;
         }
@@ -749,7 +756,7 @@ void batteryLevel()
     }
     if (hiddenFeature == true)
     {
-        if (negativeTotalSpeed < 0)
+        if (negativTotalSpeed < 0)
         {
             if (speedDistance > 20)
             { // Absoultt verdi, derfor over 20
@@ -1172,9 +1179,6 @@ void hiddenFeatureTimer()
 void menu()
 {
     hiddenFeatureTimer();
-    if (batteryHealthPercentage < 99){
-        batteryHealthPercentage = 99;
-    }
     switch (menuVar)
     {
     case 0:
